c6c6024714acdee0c118669410ad44954a1fbc45
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-07-10 15:15:13 +0300

    nav yapısı oluşturuldı

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter><App /></BrowserRouter>,
   document.getElementById('root')
 );
 
